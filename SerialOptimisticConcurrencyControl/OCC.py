bad804c1571a2cf59c064a8a9ad57c082797cd75
Roby Purnomo <74169230+robypurnomo@users.noreply.github.com>
2022-12-05 18:01:42 +0700

    fix : OCC, add test

--- SerialOptimisticConcurrencyControl/OCC.py
+++ SerialOptimisticConcurrencyControl/OCC.py
@@ -1,12 +1,20 @@
+from collections import deque
+
 lock = []
 records = []
 
 filename = input("Enter the name of the file: ")
+# filename = "test1.txt"
 file = open(filename, "r")
 text = list(file)
 records = [s.strip() for s in text]
+original = ""
+for i in range (len(records)):
+    original += records[i] + ";"
+records.reverse()
 
-transactions = {}
+transactions = {1:{"start": 1, "validation": 0, 
+            "finish": 0, "result":None, "read":[], "write":[]}}
 
 def intersect(arr1, arr2):
     res = []
@@ -16,80 +24,62 @@ def intersect(arr1, arr2):
                 res.append(arr1[i])
     return res
 
-def isEnd(transactions):
-    res = True
-    for i in range (1, len(transactions)+1):
-        if transactions[i]["finish"] == 0:
-            res = False
-    return res
-
-for i in range (len(records)):
-    current = (records[i])
+time = 1
+while (len(records) > 0) :
+    current = (records.pop())
+    msg = ""
     trx = int(current[1])
+    currComponent = ["", "", ""]
+    currComponent[0] = trx
+    currComponent[1] = current[0]
+
     if trx not in transactions.keys() :
-        transactions[trx] = {"start": i+1, "validation": 0, 
+        transactions[trx] = {"start": time, "validation": 0, 
                     "finish": 0, "result":None, "read":[], "write":[]}
 
-for transaction in transactions.keys() :
-    msg = ""
-    for i in range (len(records)):
-        current = records[i]
-        trx = int(current[1])
-        if trx == transaction :
-            time = i+1
-            current = (records[i])
-            currComponent = ["", "", ""]
-            currComponent[0] = trx
-            currComponent[1] = current[0]
-            if current[0] != "C" :
-                currComponent[2] = current[3]
+    if current[0] != "C" :
+        currComponent[2] = current[3]
 
-            # start phase
-            if currComponent[0] not in transactions.keys() :
-                transactions[currComponent[0]] = {"start": time, "validation": 0, 
-                            "finish": 0, "result":None, "read":[], "write":[]}
-            if currComponent[1] == "R":
-                if currComponent[2] not in transactions[currComponent[0]]["read"] :
-                    transactions[currComponent[0]]["read"].append(currComponent[2])
-            elif currComponent[1] == "W":
-                if currComponent[2] not in transactions[currComponent[0]]["write"] :
-                    transactions[currComponent[0]]["write"].append(currComponent[2])
+    # start phase
+    if currComponent[0] not in transactions.keys() :
+        transactions[currComponent[0]] = {"start": time, "validation": 0, 
+                    "finish": 0, "result":None, "read":[], "write":[]}
+    if currComponent[1] == "R":
+        if currComponent[2] not in transactions[currComponent[0]]["read"] :
+            transactions[currComponent[0]]["read"].append(currComponent[2])
+    elif currComponent[1] == "W":
+        if currComponent[2] not in transactions[currComponent[0]]["write"] :
+            transactions[currComponent[0]]["write"].append(currComponent[2])
 
-            # validation phase
-            if (currComponent[1] == "W") and (transactions[currComponent[0]]["result"] == None):
-                transactions[currComponent[0]]["validation"] = time
-                result = True
-                if currComponent[0] == 1:
-                    result = True
-                for j in range (1,currComponent[0]):
-                    if transactions[j]["result"] == False:
-                        msg = f"transaksi {j} gagal"
-                        result = False
-                        break
-                    if transactions[j]["finish"] < transactions[currComponent[0]]["start"] :
-                        result = True
+    # validation phase
+    if (currComponent[1] == "C"):
+        transactions[currComponent[0]]["validation"] = time
+        result = "berhasil"
+        for key in transactions.keys():
+            transaction = transactions[key]
+            if (transaction["validation"] != 0) and (transaction["validation"] < transactions[currComponent[0]]["validation"]):
+                if transaction["finish"] < transactions[currComponent[0]]["start"] :
+                    result = "berhasil"
+                else :
+                    Intersect = intersect(transaction["write"], transactions[currComponent[0]]["read"])
+                    if (transaction["finish"] < time) and (len(Intersect) == 0):
+                            result = "berhasil"
                     else :
-                        if (transactions[j]["finish"] < time):
-                            Intersect = intersect(transactions[j]["write"], transactions[currComponent[0]]["read"])
-                            if (len(Intersect) == 0):
-                                result = True
-                            else :
-                                msg = f"terjadi intersect antara transaksi {transaction} dengan transaksi {j} yaitu saat pembacaan data {Intersect}"
-                                result = False
-                                break
-                        else:
-                            msg = f"transaksi {transaction} melakukan write sebelum transaksi {j} selesai"
-                            result = False
-                            break
-                transactions[currComponent[0]]["result"] = result
+                        result = f"terjadi intersect antara transaksi {currComponent[0]} dengan transaksi {key} yaitu saat pembacaan data {Intersect}"
+                        break
+        transactions[currComponent[0]]["result"] = result
+        
+        # finish phase
+        transactions[currComponent[0]]["finish"] = time if transactions[currComponent[0]]["result"] == "berhasil" else -1
 
-            # rollback
-            if (transactions[currComponent[0]]["result"] == False):
-                p = "rollback"
+    time += 1
 
-            # finish phase
-            if (currComponent[1] == "W") and (transactions[currComponent[0]]["result"] == True):
-                transactions[currComponent[0]]["finish"] = time
-
-    hasil = "berhasil" if transactions[currComponent[0]]["result"] == True else f"gagal karena {msg}"
-    print(f"Transaksi {transaction} {hasil}")
+print(f"\nTransaksi : {original}")
+print("\nHasil :\n")
+for key in transactions.keys():
+    transaction = transactions[key]
+    msg = transaction["result"]
+    success = "- start = " + str(transaction["start"]) + "\n- validation = " + str(transaction["validation"]) + "\n- finish = " + str(transaction["finish"])
+    hasil = f"berhasil : \n{success}" if transaction["result"] == "berhasil" else f"gagal karena {msg}"
+    print(f"Transaksi {key} {hasil}")
+    print()
